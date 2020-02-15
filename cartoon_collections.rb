a7c07131731e2628d9f49f8bdaac37252a233f81
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 03:00:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,7 @@ def roll_call_dwarves(names)
   end
 end 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls# code an argument here
